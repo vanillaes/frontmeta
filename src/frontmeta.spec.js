09a60190f6469b1e8b352300471d479612a81785
Evan Plaice <evanplaice@gmail.com>
2019-12-12 22:03:58 -0700

    Fix 'Basic Usage' Test

--- src/frontmeta.spec.js
+++ src/frontmeta.spec.js
@@ -5,7 +5,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const basic = require('./__test__/basic.json');
 
-test('Creates a template from a string', (t) => {
+test('Basic Usage', (t) => {
   const input = basic.input.join('\n');
   const expect = basic.expect;
   const result = frontmeta(input);
