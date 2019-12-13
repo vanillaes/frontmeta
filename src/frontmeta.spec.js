2b2c677974316be89c0bb29e9d4cc0e26557b594
Evan Plaice <evanplaice@gmail.com>
2019-12-12 22:06:06 -0700

    Whitespace1 Test Pass

--- src/frontmeta.spec.js
+++ src/frontmeta.spec.js
@@ -4,10 +4,21 @@ import frontmeta from '../index.js';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const basic = require('./__test__/basic.json');
+const whitespace1 = require('./__test__/whitespace1.json');
 
 test('Basic Usage', (t) => {
   const input = basic.input.join('\n');
   const expect = basic.expect;
+  const result = frontmeta(input);
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
+
+test('Whitespace1 - white space before opening dashes is ignored', (t) => {
+  const input = whitespace1.input.join('\n');
+  const expect = whitespace1.expect;
   const result = frontmeta(input);
 
   t.deepEqual(result, expect);
